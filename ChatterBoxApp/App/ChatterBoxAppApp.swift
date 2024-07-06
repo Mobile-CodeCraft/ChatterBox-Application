c28424357c5f32f035be297e222ca2e6168c750a
Elizabeth Fassler <128157075+elle-concetta@users.noreply.github.com>
2024-07-06 16:58:48 -0400

    Update ChatterBoxAppApp.swift

--- ChatterBoxApp/App/ChatterBoxAppApp.swift
+++ ChatterBoxApp/App/ChatterBoxAppApp.swift
@@ -1,1 +1,15 @@
+import SwiftUI
+import Firebase
 
+@main
+struct SwiftUIChatAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
